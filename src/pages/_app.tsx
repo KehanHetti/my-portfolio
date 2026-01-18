78f22c0483b8f19e11ea90eb8a678d5bd9edb315
KehanHetti <72952362+KehanHetti@users.noreply.github.com>
2026-01-18 02:57:26 -0800

    Add Google Analytics tracking using Next.js Script component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Geist } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -12,6 +13,19 @@ const geist = Geist({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${geist.variable} font-sans antialiased`}>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5VM8E4DQ6Y"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5VM8E4DQ6Y');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </div>
